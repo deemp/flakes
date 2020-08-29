352869f1f74ae42a7aaa844cf176da50eb73bc31
Fabian Schneider <faebl.taylor@protonmail.com>
2020-08-29 11:06:12 +0200

    removed duplicate string literals

--- Converter.hs
+++ Converter.hs
@@ -1,5 +1,13 @@
 module Converter where
 
+backticks :: String 
+backticks = "```"
+
+languageMark :: String
+languageMark = backticks ++ "haskell"
+
+birdticks :: String
+birdticks = "<>"
 
 -- | converts a files contents from the LHS birdtick style 
 --   to markdown, replacing the code marked by birdticks with ```haskell ... ```
@@ -8,14 +16,14 @@ convertToMd = unlines . convert' "" . lines
   where
     convert' :: String -> [String] -> [String]
     convert' prev []
-        | isFirstOO prev "<>"          = ["```"]     -- close code tags at the end
+        | isFirstOO prev birdticks     = [backticks]     -- close code tags at the end
         | otherwise                    = []          
     convert' prev (h:t)
-        | not (isFirstOO prev "<>") &&                      -- checks for code to stars, inserts newline above code block if needed
-               isFirstOO h    "<>"     = (if prev == "" then [] else [""]) ++ ["```haskell", drop 2 h] ++ rest
-        | isFirstOO      h    "<>"     = (drop 2 h):rest    -- checks for code
-        | isFirstOO      prev "<>"     = ["```"] ++ (if h == "" then [] else [""]) ++ [h] ++ rest    -- checks for code end, insers newline after code block if needed
-        | otherwise                    = h:rest
+        | not (isFirstOO prev birdticks) &&                      -- checks for code to stars, inserts newline above code block if needed
+               isFirstOO h    birdticks      = (if prev == "" then [] else [""]) ++ [languageMark, drop 2 h] ++ rest
+        | isFirstOO      h    birdticks      = (drop 2 h):rest    -- checks for code
+        | isFirstOO      prev birdticks      = [backticks] ++ (if h == "" then [] else [""]) ++ [h] ++ rest    -- checks for code end, insers newline after code block if needed
+        | otherwise                          = h:rest
         where 
             rest = convert' h t
 
@@ -29,12 +37,16 @@ convertToLhs = unlines . convert' False False "" . lines
     convert' :: Bool -> Bool -> String -> [String] -> [String]
     convert' inCode inSample prev [] = []
     convert' inCode inSample prev (h:t)
-        | inCode   && h /= "```"       = ["> " ++ h] ++ (convert' True False h t)       -- handles code
-        | inSample && h /= "```"       = ["< " ++ h] ++ (convert' False True h t)       -- handles code sample
+        | inCode   && h /= backticks   = ["> " ++ h] ++ (convert' True False h t)       -- handles code
+        | inSample && h /= backticks   = ["< " ++ h] ++ (convert' False True h t)       -- handles code sample
         | headingN h /= 0              = [headingO h ++ (tail $ dropWhile (=='#') h) ++ headingC h] ++ rest   -- converts headings to html
-        | h == "```haskell"            = (if prev == "" then [] else [""]) ++ convert' True False prev t      -- handles code block starts, adds newline if needed
-        | (inCode || inSample) && h == "```" = (if length t > 0 && (head t) == "" then [] else [""]) ++ convert' False False prev t  -- handles code and sample block ends
-        | take 3 h == "```"            = (if prev == "" then [] else [""]) ++ convert' False True prev t      --  handles sample block starts, adds newline if needed
+        | h == languageMark            = (if prev == "" then [] else [""]) ++ convert' True False prev t      -- handles code block starts, adds newline if needed
+        | (inCode || inSample) 
+            && h == "```"              = (if length t > 0 && (head t) == "" 
+                                            then [] 
+                                            else [""]) 
+                                          ++ convert' False False prev t  -- handles code and sample block ends
+        | take 3 h == backticks        = (if prev == "" then [] else [""]) ++ convert' False True prev t      --  handles sample block starts, adds newline if needed
         | isFirstOO h ">"              = ["NOTE: " ++ drop 2 h] ++ rest                                       -- handles quotes
         | otherwise                    = h:rest
         where 
