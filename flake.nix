1b3e590218c488ba7fc6411717e8f97bb916945e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-15 15:55:29 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
-    # nixpkgs-stable.url = "github:NixOS/nixpkgs/67e45078141102f45eff1589a831aeaa3182b41e";
     nixpkgs.url = "github:NixOS/nixpkgs/0e304ff0d9db453a4b230e9386418fd974d5804a";
     flake-utils.url = "github:numtide/flake-utils";
     my-codium = {
-      url = "github:br4ch1st0chr0n3/flakes?dir=codium&rev=5317c7a3882a96bc8ee92bb4979edaac6047a183";
+      url = "github:br4ch1st0chr0n3/flakes?dir=codium&rev=1ba5800e9d39fad3f040083f586bac354cbf6c1b";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
@@ -14,7 +13,6 @@
     };
     hls = {
       url = "github:haskell/haskell-language-server/7760340e999693d07fdbea49c9e20a3dd5458ad3";
-      # inputs.nixpkgs.follows = "nixpkgs-stable";
     };
     gitignore = {
       url = "github:hercules-ci/gitignore.nix/a20de23b925fd8264fd7fad6454652e142fd7f73";
@@ -40,7 +38,7 @@
 
       manager = justStaticExecutables (callCabal2nix "manager" (gitignoreSource ./manager) { });
 
-      inherit (my-codium.packages.${system})
+      inherit (my-codium.tools.${system})
         writeSettingsJson
         settingsNix
         extensions
@@ -48,12 +46,10 @@
         mergeValues
         toList
         shellTools
-        json2nix
         ;
       writeSettings = writeSettingsJson
         {
           inherit (settingsNix) haskell todo-tree files editor gitlens git nix-ide;
-          window."window.zoomLevel" = 0.3;
           workbench = settingsNix.workbench // { "workbench.colorTheme" = "Monokai"; };
         };
     in
@@ -67,7 +63,6 @@
                 (toList { inherit (shellTools) nix; haskell = builtins.removeAttrs shellTools.haskell [ "haskell-language-server" ]; })
                 (pkgs.haskell.compiler."ghc${ghcV}")
                 codium
-                json2nix
                 writeSettings
                 manager
                 hls.packages.${system}."haskell-language-server-${ghcV}"
@@ -95,12 +90,12 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://br4ch1st0chr0n3-acpoj.cachix.org"
+      https://br4ch1st0chr0n3-nix-managed.cachix.org
       "https://br4ch1st0chr0n3-flakes.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "br4ch1st0chr0n3-acpoj.cachix.org-1:OYzTMty0XgyEIcm+o9tjKotr9ZjNeC4JCWmc0P0nx3U="
+      "br4ch1st0chr0n3-nix-managed.cachix.org-1:sDKsfgu5fCCxNwVhZg+AWeGvbLlEtZoyzkSNKRM/KAo="
       "br4ch1st0chr0n3-flakes.cachix.org-1:Dyc2yLlRIkdbq8CtfOe24QQhQVduQaezkyV8J9RhuZ8="
     ];
   };
