a3288e2ecf3933c9dc6cb49e1f9387f8ab455d17
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-22 13:48:51 +0300

    fix nixconfig

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    inputs.url = "github:br4ch1st0chr0n3/flakes?dir=inputs";
-    nixpkgs.follows = "inputs/nixpkgs";
-    flake-utils.follows = "inputs/flake-utils";
-    my-codium.follows = "inputs/my-codium";
-    flake-compat.follows = "inputs/flake-compat";
-    hls.follows = "inputs/haskell-language-server";
-    gitignore.follows = "inputs/gitignore";
+    my-inputs.url = "github:br4ch1st0chr0n3/flakes?dir=inputs";
+    nixpkgs.follows = "my-inputs/nixpkgs";
+    flake-utils.follows = "my-inputs/flake-utils";
+    my-codium.follows = "my-inputs/my-codium";
+    flake-compat.follows = "my-inputs/flake-compat";
+    hls.follows = "my-inputs/haskell-language-server";
+    gitignore.follows = "my-inputs/gitignore";
   };
   outputs =
     { self
@@ -16,7 +16,7 @@
     , flake-compat
     , hls
     , gitignore
-    , inputs
+    , my-inputs
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
@@ -97,14 +97,16 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://nix-community.cachix.org"
-      https://br4ch1st0chr0n3-nix-managed.cachix.org
-      "https://br4ch1st0chr0n3-flakes.cachix.org"
+      https://haskell-language-server.cachix.org
+      https://nix-community.cachix.org
+      https://hydra.iohk.io
+      https://br4ch1st0chr0n3.cachix.org
     ];
     extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "br4ch1st0chr0n3-nix-managed.cachix.org-1:sDKsfgu5fCCxNwVhZg+AWeGvbLlEtZoyzkSNKRM/KAo="
-      "br4ch1st0chr0n3-flakes.cachix.org-1:Dyc2yLlRIkdbq8CtfOe24QQhQVduQaezkyV8J9RhuZ8="
+      haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
+      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
     ];
   };
 }
