02e7d7d66af39e8a5e18d4ecf9ae109d4c18087e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-24 13:47:48 +0300

    upd

--- inputs/flake.nix
+++ inputs/flake.nix
@@ -32,6 +32,9 @@
     haskell-language-server = {
       url = "github:haskell/haskell-language-server/ff4f29f859244f6e828ecf74fab6e8a6b745c020";
     };
+    json2md = {
+      url = "github:br4ch1st0chr0n3/flakes?dir=json2md";
+    };
     flake-compat = {
       url = "github:edolstra/flake-compat/b4a34015c698c7793d592d66adbab377907a2be8";
       flake = false;
