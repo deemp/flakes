44db7aa3f721f857e78ab8195d90a7aa1a641ba5
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-09-26 02:17:58 +0300

    upd

--- inputs/flake.nix
+++ inputs/flake.nix
@@ -12,7 +12,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     easy-purescript-nix = {
-      url = "github:justinwoo/easy-purescript-nix/5926981701ac781f08b02e31e4705e46b799299d";
+      url = "github:justinwoo/easy-purescript-nix/3d8b602e80c0fa7d97d7f03cb8e2f8b06967d509";
       flake = false;
     };
     dream2nix = {
