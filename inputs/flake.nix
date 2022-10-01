a8b79acec2617174bc7e9a71a85e1c993520c0b2
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-01 17:34:14 +0300

    push current changes: add formatting

--- inputs/flake.nix
+++ inputs/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    source-flake.url = path:../source-flake;
-    # source-flake.url = github:br4ch1st0chr0n3/flakes?dir=source-flake;
+    # source-flake.url = path:../source-flake;
+    source-flake.url = github:br4ch1st0chr0n3/flakes?dir=source-flake;
     nixpkgs.follows = "source-flake/nixpkgs";
     flake-utils.follows = "source-flake/flake-utils";
     gitignore.follows = "source-flake/gitignore";
@@ -12,10 +12,10 @@
     haskell-language-server.follows = "source-flake/haskell-language-server";
     flake-compat.follows = "source-flake/flake-compat";
     poetry2nix.follows = "source-flake/poetry2nix";
-    my-codium.url = path:../codium;
-    # my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
-    json2md.url = path:../json2md;
-    # json2md.url = github:br4ch1st0chr0n3/flakes?dir=json2md;
+    # my-codium.url = path:../codium;
+    my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
+    # json2md.url = path:../json2md;
+    json2md.url = github:br4ch1st0chr0n3/flakes?dir=json2md;
   };
   outputs = { source-flake, ... }: { inherit (source-flake) formatter; };
 }
