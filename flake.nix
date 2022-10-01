40657cde3906e6bd6d0943636713ffbdbc68f05b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-01 17:21:59 +0300

    use current changes: push with relative paths first

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    my-inputs.url = path:./inputs;
-    # my-inputs.url = github:br4ch1st0chr0n3/flakes?dir=inputs;
+    # my-inputs.url = path:./inputs;
+    my-inputs.url = github:br4ch1st0chr0n3/flakes?dir=inputs;
     flake-utils.follows = "my-inputs/flake-utils";
     nixpkgs.follows = "my-inputs/nixpkgs";
-    my-codium.url = path:./codium;
-    # my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
+    # my-codium.url = path:./codium;
+    my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
   };
   outputs =
     { self
