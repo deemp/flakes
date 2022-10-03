312da836b501a42e222be29c0cf162646ad94fee
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-03 20:44:06 +0300

    use mktemp

--- codium/scripts/cache-devshells.fish
+++ codium/scripts/cache-devshells.fish
@@ -1,6 +1,5 @@
 # Set default location of a temp dir to save devshell profiles to
-set -q PROFILES_FOR_DEVSHELLS || set PROFILES_FOR_DEVSHELLS /tmp/__profiles_for_devshells
-mkdir -p $PROFILES_FOR_DEVSHELLS
+set -q PROFILES_FOR_DEVSHELLS || set PROFILES_FOR_DEVSHELLS $(mktemp -d -t devshells-XXXXXXXXXX)
 # get the names of devshells
 set t $( nix flake show --json | jq -r --arg cur_sys "$CURRENT_SYSTEM" '.devShells[$cur_sys]|(try keys[] catch "")' )
 
@@ -13,4 +12,4 @@ if test -n "$t";
 end
 
 # remove the temporary dir
-rm -rf $PROFILES_FOR_DEVSHELLS  
+rm -rf $PROFILES_FOR_DEVSHELLS
