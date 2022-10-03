e65c153783e58abadf51c8e5de13fa2d634df64d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-03 20:23:53 +0300

    fix

--- codium/scripts/cache-packages.fish
+++ codium/scripts/cache-packages.fish
@@ -1,10 +1,12 @@
 # set temp directory for outputs for packages
 set -q PATHS_FOR_PACKAGES || set PATHS_FOR_PACKAGES /tmp/__paths_for_packages
+mkdir -p $PATHS_FOR_PACKAGES
 
 set t $( nix flake show --json | jq -r --arg cur_sys "$CURRENT_SYSTEM" '.packages[$cur_sys]|(try keys[] catch "")' )
 
 if test -n "$t";
-    printf "%s\n" $t | xargs -I {} nix build --print-out-paths .#{} > $PATHS_FOR_PACKAGES
-    cat $PATHS_FOR_PACKAGES | xargs -I {} nix-store -qR {} | cachix-wrapped push $CACHIX_CACHE;
-    rm $PATHS_FOR_PACKAGES
+    printf "%s\n" $t | xargs -I {} nix build --print-out-paths .#{} > $PATHS_FOR_PACKAGES/tmp
+    cat $PATHS_FOR_PACKAGES/tmp | xargs -I {} nix-store -qR {} | cachix-wrapped push $CACHIX_CACHE;
 end
+
+rm -rf $PATHS_FOR_PACKAGES
