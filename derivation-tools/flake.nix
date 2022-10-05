c3560de1c06069a29f0d7b687f8f1b2ae2309a8b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-05 17:38:15 +0300

    move inputs

--- derivation-tools/flake.nix
+++ derivation-tools/flake.nix
@@ -1,10 +1,4 @@
 {
-  inputs = {
-    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
-    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
-    nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils.follows = "flake-utils_/flake-utils";
-  };
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -237,4 +231,11 @@
           writeJson;
       };
     });
+
+  inputs = {
+    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils.follows = "flake-utils_/flake-utils";
+  };
 }
