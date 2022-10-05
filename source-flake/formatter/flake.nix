721e65f2f9cb994232efc5e2e834dfa17fbda98e
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-05 20:44:20 +0300

    upd formatter

--- source-flake/formatter/flake.nix
+++ source-flake/formatter/flake.nix
@@ -1,6 +1,11 @@
 {
-  inputs.nixpkgs.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
-  outputs = { nixpkgs, self }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+  inputs = {
+    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils.follows = "flake-utils_/flake-utils";
   };
+  outputs = { nixpkgs, flake-utils, self, ... }: flake-utils.lib.eachDefaultSystem (system: {
+    formatter.system = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+  });
 }
