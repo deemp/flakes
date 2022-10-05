4403c1775dbddbb0c53424041d3f21f5f97c1b32
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-05 18:07:02 +0300

    move inputs up

--- drv-tools/flake.nix
+++ drv-tools/flake.nix
@@ -1,4 +1,11 @@
 {
+  inputs = {
+    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils.follows = "flake-utils_/flake-utils";
+  };
+  
   outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
@@ -231,11 +238,4 @@
           writeJson;
       };
     });
-
-  inputs = {
-    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
-    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
-    nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils.follows = "flake-utils_/flake-utils";
-  };
 }
