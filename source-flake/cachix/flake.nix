bb10e2549a7c0f19187dcaf30e11e85871bf9bf7
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-05 14:11:14 +0300

    upd pins

--- source-flake/cachix/flake.nix
+++ source-flake/cachix/flake.nix
@@ -1,4 +1,6 @@
 {
-  inputs.cachix.url = "github:cachix/cachix/12bb254de9acf35e0859d63c81825006921e7291";
+  inputs = {
+    cachix.url = "github:cachix/cachix/12bb254de9acf35e0859d63c81825006921e7291";
+  };
   outputs = x: { };
 }
--- source-flake/nix-vscode-marketplace/flake.nix
+++ source-flake/nix-vscode-marketplace/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nix-vscode-marketplace = {
-    url = "github:br4ch1st0chr0n3/vscodium-extensions/fix-ci/";
+    url = "github:br4ch1st0chr0n3/vscodium-extensions/fix-ci";
     inputs.nixpkgs.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
   };
   outputs = x: { };
