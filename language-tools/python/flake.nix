fa0271cb6aef05d3f961e0a69fa1aa14fbf04d81
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-08 22:35:32 +0300

    add function

--- language-tools/python/flake.nix
+++ language-tools/python/flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
     flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    drv-tools.url = github:br4ch1st0chr0n3/flakes?dir=drv-tools;
     nixpkgs.follows = "nixpkgs_/nixpkgs";
     flake-utils.follows = "flake-utils_/flake-utils";
   };
@@ -18,10 +19,27 @@
         ${builtins.readFile ./scripts/activate.sh}
         set +e
       '';
+      inherit (drv-tools.functions.${system}) runInEachDir;
+      createVenvs = dirs: runInEachDir
+        {
+          name = "create-venvs";
+          message = "creating environment in";
+          inherit dirs;
+          command = ''
+            ${activateVenv}
+            poetry install --no-root
+          '';
+          runtimeInputs = [ pkgs.poetry ];
+          longDescription = ''
+            Create Python `.venv`s via `poetry`.
+          '';
+        };
     in
     {
       snippets = {
-        inherit activateVenv;
+        inherit
+          activateVenv
+          createVenvs;
       };
     }
     );
