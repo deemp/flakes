3ef32bef9ae713d6c83e78049c97012be973d691
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-11 13:30:41 +0300

    uncomment cachix substituters

--- codium/flake.nix
+++ codium/flake.nix
@@ -120,15 +120,15 @@
   nixConfig = {
     extra-substituters = [
       https://haskell-language-server.cachix.org
-      # https://nix-community.cachix.org
+      https://nix-community.cachix.org
       https://hydra.iohk.io
-      # https://br4ch1st0chr0n3.cachix.org
+      https://br4ch1st0chr0n3.cachix.org
     ];
     extra-trusted-public-keys = [
       haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
-      # nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
       hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
-      # br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
+      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
     ];
   };
 }
