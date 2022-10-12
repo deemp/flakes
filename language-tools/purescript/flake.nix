db6a812581cb5d760ad2aa1e24bd5f1baf16da1b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-12 22:46:56 +0300

    add purescript tools

---
+++ language-tools/purescript/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils.follows = "flake-utils_/flake-utils";
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tools = {
+        inherit (pkgs) dhall-lsp-server spago nodejs-16_x purescript;
+        inherit (pkgs.nodePackages) purescript-language-server purs-tidy;
+      };
+    in
+    {
+      packages = tools;
+    }
+    );
+}
