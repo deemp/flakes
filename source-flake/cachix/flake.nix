73f2ae7182db5316e45ebb615a07bd8ed765f189
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-17 01:03:33 +0300

    update cachix

--- source-flake/cachix/flake.nix
+++ source-flake/cachix/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    cachix.url = "github:cachix/cachix/12bb254de9acf35e0859d63c81825006921e7291";
+    cachix.url = "github:cachix/cachix/f6fe5df6d96e45359a1e5c448272cfc7e0566da0";
   };
   outputs = x: { };
 }
