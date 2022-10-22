ff55af62c0c834c84e6187c4fd01b24f7d1f1193
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-23 00:43:12 +0300

    rm nixpkgs

--- source-flake/gitignore/flake.nix
+++ source-flake/gitignore/flake.nix
@@ -1,7 +1,6 @@
 {
   inputs.gitignore = {
     url = "github:hercules-ci/gitignore.nix/a20de23b925fd8264fd7fad6454652e142fd7f73";
-    inputs.nixpkgs.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
   };
   outputs = x: { };
 }
