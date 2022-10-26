447d3a420c713d9a6536e53e4b370677cfb365e8
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-10-26 21:57:24 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    flake-tools.url = github:br4ch1st0chr0n3/flakes?dir=flake-tools;
+    drv-tools.url = github:br4ch1st0chr0n3/flakes?dir=drv-tools;
+    nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils.follows = "flake-utils_/flake-utils";
+    formatter.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/formatter;
+    my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
+    python-tools.url = github:br4ch1st0chr0n3/flakes?dir=language-tools/python;
+  };
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , flake-tools
+    , drv-tools
+    , my-codium
+    , formatter
+    , python-tools
+    , ...
+    }: flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        inherit (my-codium.configs.${system}) extensions;
+        inherit (my-codium.functions.${system}) mkCodium;
+        inherit (drv-tools.functions.${system}) mkShellApp;
+        inherit (python-tools.functions.${system}) createVenvs;
+        inherit (python-tools.snippets.${system}) activateVenv;
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        codium = mkCodium {
+          inherit extensions;
+          runtimeDependencies = [
+            (
+              builtins.attrValues
+                {
+                  inherit (pkgs)
+                    docker poetry direnv rnix-lsp
+                    nixpkgs-fmt fish;
+                }
+            )
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = activateVenv;
+        };
+        packages = {
+          default = codium;
+          createVenvs = createVenvs [ "." ];
+        };
+      });
+
+  nixConfig = {
+    extra-trusted-substituters = [
+      https://haskell-language-server.cachix.org
+      https://nix-community.cachix.org
+      https://hydra.iohk.io
+      https://br4ch1st0chr0n3.cachix.org
+    ];
+    extra-trusted-public-keys = [
+      haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
+      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
+    ];
+  };
+}
