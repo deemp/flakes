b81c67de0a481445803f099b4c63b9cdc6f7fa18
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-07 21:32:19 +0300

    fix gitignore

--- source-flake/gitignore/flake.nix
+++ source-flake/gitignore/flake.nix
@@ -1,6 +1,10 @@
 {
-  inputs.gitignore = {
-    url = "github:hercules-ci/gitignore.nix/a20de23b925fd8264fd7fad6454652e142fd7f73";
+  inputs = {
+    nixpkgs_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix/a20de23b925fd8264fd7fad6454652e142fd7f73";
+      inputs.nixpkgs.follows = "nixpkgs_/nixpkgs";
+    };
   };
   outputs = x: { };
 }
