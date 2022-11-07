4a788d619e3d4b35847f2bc360b363ef911a3329
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-07 16:21:01 +0300

    refactor flakes

---
+++ source-flake/vscode-extensions-selected/flake.nix
@@ -0,0 +1,4 @@
+{
+  inputs.vscode-extensions-selected.url = github:br4ch1st0chr0n3/nix-vscode-extensions/selected;
+  outputs = x: {};
+}
--- source-flake/vscode-extensions/flake.nix
+++ source-flake/vscode-extensions/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nix-vscode-marketplace = {
-    url = "github:br4ch1st0chr0n3/vscodium-extensions/fix-ci";
+    url = "github:br4ch1st0chr0n3/vscodium-extensions/master";
   };
   outputs = x: { };
 }
