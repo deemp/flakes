e81ff002da0bdea7e5858443fab7b464c10890f5
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-08 13:49:52 +0300

    lock vscode extensions

--- source-flake/vscode-extensions-selected/flake.nix
+++ source-flake/vscode-extensions-selected/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions-selected.url = github:br4ch1st0chr0n3/nix-vscode-extensions/selected;
+  inputs.vscode-extensions-selected.url = "github:br4ch1st0chr0n3/nix-vscode-extensions/b927db88c79e461e1e6e8ba75e1ad3c512c4b73a";
   outputs = x: {};
 }
--- source-flake/vscode-extensions/flake.nix
+++ source-flake/vscode-extensions/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.vscode-extensions = {
-    url = "github:br4ch1st0chr0n3/nix-vscode-extensions/master";
+    url = "github:br4ch1st0chr0n3/nix-vscode-extensions/8f37966352f94e0a76d8daef18b00949e555fdc4";
   };
   outputs = x: { };
 }
