8f41a8b579a95b518babd0d86744e21be4da0bad
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-10 12:54:55 +0300

    try script

--- week07/scripts/deploy.ts
+++ week07/scripts/deploy.ts
@@ -1,21 +1,58 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const Collection = await ethers.getContractFactory("Collection")
+  const NFTCollection = await ethers.getContractFactory("NFTCollection")
   const Rewards = await ethers.getContractFactory("Rewards")
   const NFTStaking = await ethers.getContractFactory("NFTStaking")
 
-  const collection = await Collection.deploy()
-  const rewards = await Rewards.deploy()
-  const nftStaking = await NFTStaking.deploy(collection.address, rewards.address)
+  const accounts = await ethers.getSigners()
+  const owner = accounts[0]
+  const acc1 = accounts[1]
 
-  await nftStaking.deployed()
+  const collection1 = await NFTCollection.connect(owner).deploy("Collection1", "C1")
+  // const collection2 = await NFTCollection.deploy("Collection2", "C2")
 
-  await rewards.addController(nftStaking.address)
+  // console.log(await collection1.owner())
+  const nftStaking = await NFTStaking.connect(owner).deploy()
+  const rewards1 = await Rewards.connect(owner).deploy()
+  
+  // const rewards2 = await Rewards.connect(owner).deploy()
+  
+  await collection1.connect(owner).setApprovalForAll(nftStaking.address, true)
+  await rewards1.connect(owner).addController(nftStaking.address)
+  await rewards1.connect(owner).addController(owner.address)
+  
+  await nftStaking.addVault(collection1.address, rewards1.address, "vault1")
+  // await nftStaking.addVault(collection2.address, rewards2.address, "vault2")
 
-  console.log(`Collection: ${collection.address}`)
-  console.log(`Rewards: ${rewards.address}`)
-  console.log(`NFTStaking: ${nftStaking.address}`)
+  
+  // TODO fix Error: VM Exception while processing transaction: reverted with reason string 'ERC721: caller is not token owner or approved
+  await collection1.connect(owner).mint(acc1.address, await collection1.maxMintAmount())
+  
+
+  // console.log(await (await nftStaking.multiVault(0)).collection.)
+    // .collection1.connect(acc1).walletOfOwner(acc1.address))
+  
+  // await collection2.mint(acc1.address, await collection2.maxMintAmount())
+  // await collection1.mint(acc2.address, 2)
+  
+  // await nftStaking.connect(acc2).stake(1, [1,2,3,4,5,6,7,8,9,10])
+  // await nftStaking.connect(acc1).stake(0, [1])
+  // console.log(await nftStaking.connect(acc1).balanceOf(acc1.address, 1))
+  // await nftStaking.connect(acc1).balanceOf(acc1.address, 1)
+  
+  
+  // const rewards = await Rewards.deploy()
+  // const nftStaking = await NFTStaking.deploy()
+
+  // await nftStaking.deployed()
+
+  // await rewards.addController(nftStaking.address)
+
+  // console.log(`Collection: ${collection1.address}`)
+  // console.log(`Collection: ${collection2.address}`)
+  // console.log(`Rewards: ${rewards.address}`)
+  // console.log(`NFTStaking: ${nftStaking.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
