0abded1ed2e4e8a7ece339d2331576e2467d4a17
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-10 20:20:34 +0300

    refactor a bit

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
   inputs = {
-    nixpkgs_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs;
+    nixpkgs_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
     nixpkgs.follows = "nixpkgs_/nixpkgs";
-    flake-utils_.url = github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils;
+    flake-utils_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils";
     flake-utils.follows = "flake-utils_/flake-utils";
-    my-codium.url = github:br4ch1st0chr0n3/flakes?dir=codium;
-    drv-tools.url = github:br4ch1st0chr0n3/flakes?dir=drv-tools;
-    flake-tools.url = github:br4ch1st0chr0n3/flakes?dir=flake-tools;
-    haskell-tools.url = github:br4ch1st0chr0n3/flakes?dir=language-tools/haskell;
+    my-codium.url = "github:br4ch1st0chr0n3/flakes?dir=codium";
+    drv-tools.url = "github:br4ch1st0chr0n3/flakes?dir=drv-tools";
+    flake-tools.url = "github:br4ch1st0chr0n3/flakes?dir=flake-tools";
+    haskell-tools.url = "github:br4ch1st0chr0n3/flakes?dir=language-tools/haskell";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
   outputs =
     { self
@@ -31,20 +35,16 @@
         extensions
         settingsNix
         ;
-      inherit (drv-tools.functions.${system})
-        toList
-        mkBin
-        ;
       inherit (flake-tools.functions.${system})
         mkFlakesUtils
         ;
       inherit (haskell-tools.functions.${system})
         toolsGHC
         ;
-      inherit (toolsGHC ghcVersion) stack callCabal staticExecutable hls;
+      inherit (toolsGHC ghcVersion) stack staticExecutable hls;
 
       manager =
-        let 
+        let
           manager_ = "manager";
           manager-exe = staticExecutable manager_ ./${manager_};
         in
@@ -66,11 +66,11 @@
         inherit (settingsNix) haskell todo-tree files editor gitlens git nix-ide workbench;
       };
 
-      tools = [ manager stack hls ];
+      tools = [ manager stack hls pkgs.ghcid ];
 
       codium = mkCodium {
         extensions = { inherit (extensions) nix haskell misc github; };
-        runtimeDependencies = [ stack hls pkgs.rnix-lsp pkgs.ghcid ];
+        runtimeDependencies = [ stack hls ];
       };
 
       flakesUtils = mkFlakesUtils [ "." ];
@@ -87,7 +87,7 @@
         {
           default = pkgs.mkShell {
             name = "dev-tools";
-            buildInputs = tools;
+            buildInputs = tools ++ [ codium ];
             shellHook = ''
               source <(manager --bash-completion-script `which manager`)
             '';
@@ -109,16 +109,16 @@
 
   nixConfig = {
     extra-substituters = [
-      https://haskell-language-server.cachix.org
-      https://nix-community.cachix.org
-      https://hydra.iohk.io
-      https://br4ch1st0chr0n3.cachix.org
+      "https://haskell-language-server.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://hydra.iohk.io"
+      "https://br4ch1st0chr0n3.cachix.org"
     ];
     extra-trusted-public-keys = [
-      haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
-      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
-      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
-      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
+      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU="
     ];
   };
 }
