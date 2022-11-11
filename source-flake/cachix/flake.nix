b038956b786c31ba96d38162d196142531f2c525
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-11 13:56:33 +0300

    simplify flakes

--- source-flake/cachix/flake.nix
+++ source-flake/cachix/flake.nix
@@ -1,6 +1,4 @@
 {
-  inputs = {
-    cachix.url = "github:cachix/cachix/f6fe5df6d96e45359a1e5c448272cfc7e0566da0";
-  };
+  inputs.cachix.url = "github:cachix/cachix/f6fe5df6d96e45359a1e5c448272cfc7e0566da0";
   outputs = x: { };
 }
--- source-flake/dream2nix/flake.nix
+++ source-flake/dream2nix/flake.nix
@@ -1,6 +1,4 @@
 {
-  inputs.dream2nix = {
-    url = "github:nix-community/dream2nix/785b76894263fd76a0e69955e4cd990b7b735bdf";
-  };
+  inputs.dream2nix.url = "github:nix-community/dream2nix/785b76894263fd76a0e69955e4cd990b7b735bdf";
   outputs = x: { };
 }
