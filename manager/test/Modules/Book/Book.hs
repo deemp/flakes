fdc575dd5c3ef6e230542bdbd3b9f1d50dc329e5
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-12 22:26:01 +0300

    mk test repo

---
+++ manager/test/Modules/Book/Book.hs
@@ -0,0 +1,13 @@
+module Book(getDataDir) where
+
+import Prelude ()
+import Relude
+
+import qualified System.Directory as Dir
+import System.FilePath ((</>))
+
+getDataDir :: IO FilePath
+getDataDir = do
+  dir <- Dir.getXdgDirectory Dir.XdgData "sockets-and-pipes"
+  Dir.createDirectoryIfMissing True dir
+  return dir
