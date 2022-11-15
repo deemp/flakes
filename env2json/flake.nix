0074f3efe686fb74c9727558c43786c7be3bd86f
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-15 16:59:58 +0300

    reorder inputs

--- env2json/flake.nix
+++ env2json/flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
     nixpkgs_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
-    flake-utils_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils";
     nixpkgs.follows = "nixpkgs_/nixpkgs";
+    flake-utils_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils";
     flake-utils.follows = "flake-utils_/flake-utils";
   };
   outputs =
