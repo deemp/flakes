a45001fbe694dead1b9524a3ed1294720882d8f3
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-20 22:11:30 +0300

    rename

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Development
 
-1. Install Nix - complete all [steps](https://github.com/br4ch1st0chr0n3/flakes/blob/main/README/InstallNix.md)
+1. Install Nix - complete all [steps](https://github.com/deemp/flakes/blob/main/README/InstallNix.md)
 2. Open VSCodium
 
    ```console
--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
   inputs = {
-    nixpkgs_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
-    flake-utils_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils";
-    flakes-tools.url = "github:br4ch1st0chr0n3/flakes?dir=flakes-tools";
-    drv-tools.url = "github:br4ch1st0chr0n3/flakes?dir=drv-tools";
+    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
+    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
+    flakes-tools.url = "github:deemp/flakes?dir=flakes-tools";
+    drv-tools.url = "github:deemp/flakes?dir=drv-tools";
     nixpkgs.follows = "nixpkgs_/nixpkgs";
     flake-utils.follows = "flake-utils_/flake-utils";
-    formatter.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/formatter";
-    my-codium.url = "github:br4ch1st0chr0n3/flakes?dir=codium";
-    vscode-extensions_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/vscode-extensions";
+    formatter.url = "github:deemp/flakes?dir=source-flake/formatter";
+    my-codium.url = "github:deemp/flakes?dir=codium";
+    vscode-extensions_.url = "github:deemp/flakes?dir=source-flake/vscode-extensions";
     vscode-extensions.follows = "vscode-extensions_/vscode-extensions";
-    python-tools.url = "github:br4ch1st0chr0n3/flakes?dir=language-tools/python";
-    my-devshell.url = "github:br4ch1st0chr0n3/flakes?dir=devshell";
+    python-tools.url = "github:deemp/flakes?dir=language-tools/python";
+    my-devshell.url = "github:deemp/flakes?dir=devshell";
   };
   outputs =
     { self
@@ -95,16 +95,16 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      https://haskell-language-server.cachix.org
-      https://nix-community.cachix.org
-      https://hydra.iohk.io
-      https://br4ch1st0chr0n3.cachix.org
+      "https://haskell-language-server.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://hydra.iohk.io"
+      "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
-      haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8=
-      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
-      hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=
-      br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU=
+      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
     ];
   };
 }
