97257a4d57068da909a9c9e9ec3fa4aee3e24e73
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-20 22:55:23 +0300

    rename

--- flake.nix
+++ flake.nix
@@ -1,43 +1,26 @@
 {
   inputs = {
-    nixpkgs_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/nixpkgs";
+    nixpkgs_.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
     nixpkgs.follows = "nixpkgs_/nixpkgs";
-    my-codium.url = "github:br4ch1st0chr0n3/flakes?dir=codium";
-    drv-tools.url = "github:br4ch1st0chr0n3/flakes?dir=drv-tools";
-    flake-utils_.url = "github:br4ch1st0chr0n3/flakes?dir=source-flake/flake-utils";
+    my-codium.url = "github:deemp/flakes?dir=codium";
+    flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
     flake-utils.follows = "flake-utils_/flake-utils";
-    haskell-tools.url = "github:br4ch1st0chr0n3/flakes?dir=language-tools/haskell";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    my-devshell.url = "github:deemp/flakes?dir=devshell";
   };
   outputs =
     { self
     , flake-utils
     , nixpkgs
     , my-codium
-    , drv-tools
-    , haskell-tools
+    , my-devshell
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      inherit (my-codium.functions.${system})
-        writeSettingsJSON
-        mkCodium
-        ;
-      inherit (drv-tools.functions.${system})
-        mkBinName
-        ;
-      devshells = my-codium.functions.${system};
-      inherit (my-codium.configs.${system})
-        extensions
-        settingsNix
-        ;
-      devshell = my-codium.devshell.${system};
-
+      inherit (my-codium.functions.${system}) writeSettingsJSON mkCodium;
+      inherit (my-codium.configs.${system}) extensions settingsNix;
+      devshell = my-devshell.devshell.${system};
       writeSettings = writeSettingsJSON {
         inherit (settingsNix) todo-tree files editor gitlens
           git nix-ide workbench markdown-all-in-one;
@@ -65,8 +48,7 @@
         {
           packages = [ codium ] ++ tools;
           bash = {
-            extra = ''
-            '';
+            extra = '''';
           };
           commands = [
             {
@@ -85,13 +67,13 @@
       "https://haskell-language-server.cachix.org"
       "https://nix-community.cachix.org"
       "https://hydra.iohk.io"
-      "https://br4ch1st0chr0n3.cachix.org"
+      "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
       "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "br4ch1st0chr0n3.cachix.org-1:o1FA93L5vL4LWi+jk2ECFk1L1rDlMoTH21R1FHtSKaU="
+      "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
     ];
   };
 }
