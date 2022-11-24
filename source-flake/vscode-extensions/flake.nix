d72a04dc3e11d750b90daf8aad5d26464f93657b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-24 10:58:29 +0300

    unlock extensions

--- source-flake/vscode-extensions/flake.nix
+++ source-flake/vscode-extensions/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions/a168955c015f99a79586611bb9fd88ea68c452dd";
+  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions";
   outputs = x: { };
 }
