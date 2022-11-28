1732d6ea8640d21508c96db362fab9aa8436300c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-28 19:58:46 +0300

    lock extensions

--- source-flake/vscode-extensions/flake.nix
+++ source-flake/vscode-extensions/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions";
+  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions/5ecfef9e1c15015e83e1e9253d7fc0eee5c9c3dd";
   outputs = x: { };
 }
