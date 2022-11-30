b9cebe181ded885cd5c4e7895bebd6e0075f59ad
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-11-30 14:03:22 +0300

    pin extensions

--- source-flake/vscode-extensions/flake.nix
+++ source-flake/vscode-extensions/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions/5ecfef9e1c15015e83e1e9253d7fc0eee5c9c3dd";
+  inputs.vscode-extensions.url = "github:deemp/nix-vscode-extensions/28713238011e6fc40a4a27a88e78c0cc57ac288b";
   outputs = x: { };
 }
