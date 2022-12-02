6f81f8e4d5fbd07b71d24922f740d5cc7dcc11b1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2022-12-02 21:23:57 +0300

    rm modules

--- Modules/ChaptersA/C_0.hs
+++
@@ -1,9 +0,0 @@
-module C_0 (getDataDir) where
-
-import System.Directory qualified as Dir
-
-getDataDir :: IO FilePath
-getDataDir = do
-    dir <- Dir.getXdgDirectory Dir.XdgData "sockets-and-pipes"
-    Dir.createDirectoryIfMissing True dir
-    return dir
--- Modules/ChaptersA/C_1_4.hs
+++
@@ -1,7 +0,0 @@
-module C_1_4 where
-
-import Control.Monad.IO.Class (MonadIO (..))
-import System.IO qualified as IO
-
-helloWorld :: MonadIO m => m ()
-helloWorld = liftIO (IO.putStrLn "hello, world")
