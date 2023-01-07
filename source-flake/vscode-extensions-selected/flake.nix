8666390e2881e1c28d6a207057b595b745e76408
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-01-08 00:18:03 +0300

    upd extensions

--- source-flake/vscode-extensions-selected/flake.nix
+++ source-flake/vscode-extensions-selected/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions-selected.url = "github:deemp/nix-vscode-extensions/b927db88c79e461e1e6e8ba75e1ad3c512c4b73a";
+  inputs.vscode-extensions-selected.url = "github:deemp/nix-vscode-extensions/55d2f36aa4c945fa3bd421f67a81ffd3574c1507";
   outputs = x: { };
 }
