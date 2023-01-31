64aade48b0282632e38c35c0961be8556cdddcf1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-01-31 12:57:08 +0300

    edit source flakes

--- source-flake/haskell-language-server/flake.nix
+++
@@ -1,7 +0,0 @@
-{
-  inputs.haskell-language-server = {
-    url = "github:haskell/haskell-language-server/ff4f29f859244f6e828ecf74fab6e8a6b745c020";
-    inputs.nixpkgs.url = "github:deemp/flakes?dir=source-flake/nixpkgs";
-  };
-  outputs = x: { };
-}
