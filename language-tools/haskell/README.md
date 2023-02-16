6b1e70f3be47a732cd6ead21c5247c11b4c7606b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-02-16 18:14:44 +0300

    improve flake, use attrset argument

--- language-tools/haskell/README.md
+++ language-tools/haskell/README.md
@@ -1,3 +1,4 @@
 # Haskell
 
 - `hpack` version should correspond to the `stack`'s `hpack` version
+- Use `cabal v1-*` commands (see the [issue](https://github.com/NixOS/nixpkgs/issues/130556#issuecomment-1114239002))
--- language-tools/haskell/flake.nix
+++ language-tools/haskell/flake.nix
@@ -26,16 +26,16 @@
       # With haskell packages that are dependencies of the given packages
       ghcGHC = ghcVersion: override: packages:
         ((haskellPackagesGHCOverride ghcVersion override).ghcWithPackages
-          (ps: haskellDepsPackages (packages ps))) // { pname = "ghc"; };
+          (ps: haskellDepsPackages (packages ps))) // { pname = "ghc${ghcVersion}"; };
 
       # build tool with GHC of a specific version available on PATH
-      buildToolWithFlagsGHC = name: drv: flags: ghcVersion: override: packages: deps_:
+      buildToolWithFlagsGHC = name: drv: flags: ghcVersion: override: packages: runtimeDependencies:
         assert builtins.isString name && builtins.isString ghcVersion
           && builtins.isAttrs drv && builtins.isList flags;
         let
           deps = [
             (ghcGHC ghcVersion override packages)
-          ] ++ deps_;
+          ] ++ runtimeDependencies;
           flags_ = concatMapStringsNewline (x: x + " \\") flags;
         in
         withAttrs
@@ -59,11 +59,15 @@
             inherit (drv) meta;
           };
 
-      # get packages-deps for a Haskell package
+      # get deps for a Haskell package
       haskellDeps = drv: concatLists (attrValues drv.getCabalDeps);
 
-      # get packages-deps for a list of Haskell packages
-      # filter out input packages
+      # get deps for a list of Haskell packages
+      # Example. We develop packages A and B, where B is in deps of A.
+      # We don't want B in the final list of deps 
+      # because `ghcWithPackages` may fail due to errors in B.
+      # That's why, we remove the supplied packages (A and B in this example).
+      # from the final list of deps
       haskellDepsPackages = packages:
         pkgs.lib.lists.subtractLists
           packages
@@ -113,70 +117,88 @@
       # tools for a specific GHC version and overriden haskell packages for this GHC
       # see what you need to pass to your shell for GHC
       # https://docs.haskellstack.org/en/stable/nix_integration/#supporting-both-nix-and-non-nix-developers
-      toolsGHC = ghcVersion: override: packages: depsBin:
+      toolsGHC =
         {
-          hls = hlsGHC ghcVersion;
-          stack = stackWithFlagsGHCOverride ghcVersion override packages depsBin;
-          cabal = cabalWithFlagsGHCOverride ghcVersion override packages depsBin;
-          ghc = ghcGHC ghcVersion override packages;
-          justStaticExecutable = justStaticExecutableGHCOverrideDeps depsBin;
-          inherit (haskellPackagesGHC ghcVersion)
-            implicit-hie ghcid hpack
-            callCabal2nix ghcWithPackages;
-          haskellPackages = haskellPackagesGHCOverride ghcVersion override;
+          # GHC version as it's set in nixpkgs
+          # `925` is for `pkgs.haskell.packages.ghc925`
+          version ? "925"
+        , # override for haskell packages. See https://nixos.wiki/wiki/Haskell#Overrides
+          override ? { }
+        , # a function from an attrset Haskell packages to a list of packages that you develop
+          # packages that you develop should be provided in the override
+          packages ? (_: [ ])
+        , # what should be available on PATH to a Haskell app at runtime
+          runtimeDependencies ? [ ]
+        }:
+        {
+          hls = hlsGHC version;
+          stack = stackWithFlagsGHCOverride version override packages runtimeDependencies;
+          cabal = cabalWithFlagsGHCOverride version override packages runtimeDependencies;
+          ghc = ghcGHC version override packages;
+          justStaticExecutable = justStaticExecutableGHCOverrideDeps runtimeDependencies;
+          inherit (haskellPackagesGHC version) implicit-hie ghcid hpack callCabal2nix;
+          haskellPackages = haskellPackagesGHCOverride version override;
           inherit haskellDeps haskellDepsPackages;
         };
-
-      toolsGHC_ = ghcVersion: toolsGHC ghcVersion
-        {
-          overrides = self: super: {
-            haskell = pkgs.haskell.lib.overrideCabal
-              (super.callCabal2nix "haskell" ./. {
-                # you can put other packages here like
-                # lzma = super.lzma;
-              })
-              (_: {
-                # see what can be overriden - https://github.com/NixOS/nixpkgs/blob/0ba44a03f620806a2558a699dba143e6cf9858db/pkgs/development/haskell-modules/generic-builder.nix#L13
-                librarySystemDepends = [ pkgs.zlib ];
-              });
-          };
-        }
-        (ps: [ ps.haskell ])
-        [ pkgs.hello ]
-      ;
-
-      ghcVersion_ = "92";
-
-      tools =
-        let hp = toolsGHC_ ghcVersion_; in
-        {
-          inherit (toolsGHC_ ghcVersion_)
-            cabal stack hls ghc implicit-hie ghcid hpack;
-          hello-world = hp.justStaticExecutable "hello-world" hp.haskellPackages.haskell;
-        };
-
-      # deps that can be overriden in a package
-      deps_ = (toolsGHC_ ghcVersion_).haskellPackages.haskell.getCabalDeps;
     in
     {
       functions = {
         inherit toolsGHC;
       };
-
-      # test stack has `hello` on PATH
-      devShells =
-        {
-          default = pkgs.mkShell {
-            buildInputs = attrValues tools;
-            shellHook = ''
-              printf "\n--- cabal runs---\n"
-              cabal run
-
-              printf "\n--- haskell executable runs ---\n"
-              ${tools.hello-world}/bin/${tools.hello-world.pname}
-            '';
+    }
+    # Tests
+    // (
+      let
+        toolsGHC_ = version: toolsGHC {
+          inherit version;
+          override = {
+            overrides = self: super: {
+              haskell = pkgs.haskell.lib.overrideCabal
+                (super.callCabal2nix "haskell" ./. {
+                  # here can be the local packages that 
+                  # `haskell` package depends on
+                })
+                (x: {
+                  # see what can be overriden - https://github.com/NixOS/nixpkgs/blob/0ba44a03f620806a2558a699dba143e6cf9858db/pkgs/development/haskell-modules/generic-builder.nix#L13
+                  # include the previous deps
+                  librarySystemDepends = [ pkgs.zlib ] ++ (x.librarySystemDepends or [ ]);
+                });
+            };
           };
+          packages = (ps: [ ps.haskell ]);
+          runtimeDependencies = [ pkgs.hello ];
         };
-      inherit deps_ tools;
-    });
+
+        ghcVersion_ = "925";
+
+        tools =
+          let hp = toolsGHC_ ghcVersion_; in
+          {
+            inherit (toolsGHC_ ghcVersion_)
+              cabal stack hls ghc implicit-hie ghcid hpack;
+            hello-world = hp.justStaticExecutable "hello-world" hp.haskellPackages.haskell;
+          };
+
+        # deps that can be overriden in a package
+        deps_ = (toolsGHC_ ghcVersion_).haskellPackages.haskell.getCabalDeps;
+      in
+      {
+        # test stack has `hello` on PATH
+        devShells =
+          {
+            default = pkgs.mkShell {
+              buildInputs = attrValues tools;
+              shellHook = ''
+                printf "\n--- cabal runs ---\n"
+                cabal run
+
+                printf "\n--- Haskell executable runs ---\n"
+                ${tools.hello-world}/bin/${tools.hello-world.pname}
+              '';
+            };
+          };
+        inherit deps_ tools;
+      }
+    )
+    );
 }
