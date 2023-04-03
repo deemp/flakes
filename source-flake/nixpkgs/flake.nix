7e23b0e6ea239cfa9e30c6bc243be92f2fe0c6e4
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-04-03 10:31:57 +0300

    upd nixpkgs

--- source-flake/nixpkgs/flake.nix
+++ source-flake/nixpkgs/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/f379fc011c1682646f88e6030adb91f5c519c486";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/b15e0f6c64eb252e232a46b891f115c3e13c9954";
   outputs = x: { };
 }
