11ddf032957ed40ef3d89647fdba33d04344c985
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-04-15 16:51:47 +0300

    update extensions

--- source-flake/nix-vscode-extensions/flake.nix
+++ source-flake/nix-vscode-extensions/flake.nix
@@ -1,4 +1,4 @@
 {
-  inputs.vscode-extensions.url = "github:nix-community/nix-vscode-extensions/e4b75f0f77a31f42deef94bfbc10c4a48a11717f";
+  inputs.vscode-extensions.url = "github:nix-community/nix-vscode-extensions/47f6ae0253083fcced205fe2ad1acc62c49e8709";
   outputs = x: { };
 }
