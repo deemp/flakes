bba922dd1713b754394e1258e199ce4041098c68
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-05 11:04:05 +0300

    upd purescript

--- language-tools/purescript/flake.nix
+++ language-tools/purescript/flake.nix
@@ -4,24 +4,22 @@
     nixpkgs.follows = "nixpkgs_/nixpkgs";
     flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
     flake-utils.follows = "flake-utils_/flake-utils";
+    nixpkgs-purescript.url = "github:deemp/nixpkgs";
   };
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , ...
-    }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      shellTools = {
-        inherit (pkgs) dhall-lsp-server nodejs-16_x purescript;
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      pkgs-purescript = inputs.nixpkgs-purescript.legacyPackages.${system};
+      packages = {
+        inherit (pkgs-purescript) purescript;
+        inherit (pkgs) dhall-lsp-server nodejs_18;
         inherit (pkgs.nodePackages) purescript-language-server purs-tidy;
         spago = pkgs.lib.meta.addMetaAttrs { description = "PureScript build tool and package manager"; } pkgs.spago;
       };
     in
     {
-      inherit shellTools;
+      inherit packages;
     }
     );
 }
