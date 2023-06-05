9c24ff6ccbd83e4c3bb2642fa6369a94c9529d53
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-05 13:44:51 +0300

    rm unrelated substituters

--- templates/codium/generic/flake.nix
+++ templates/codium/generic/flake.nix
@@ -76,13 +76,11 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      "https://haskell-language-server.cachix.org"
       "https://nix-community.cachix.org"
       "https://cache.iog.io"
       "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
