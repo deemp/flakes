85ed3518459256e01c7d11c2de75058dc53158af
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-10 14:39:52 +0300

    upd workflows

--- workflows/flake.nix
+++ workflows/flake.nix
@@ -141,9 +141,19 @@
       # Flake directories -> step to cache based on flake files
       cacheNixDirs =
         { flakeDirs ? [ "." ], store ? nixStoreLinux, keySuffix ? "", checkIsRunnerLinux ? false, restoreOnly ? true }:
-        cacheNixFiles ({ flakeFiles = (__concatMap (x: [ "${x}/flake.nix" "${x}/flake.lock" ]) flakeDirs); inherit keySuffix store checkIsRunnerLinux restoreOnly; });
+        cacheNixFiles ({
+          flakeFiles = (__concatMap (x: [ "${x}/flake.nix" "${x}/flake.lock" ]) flakeDirs);
+          inherit keySuffix store checkIsRunnerLinux restoreOnly;
+        });
 
-      installNix = { store ? nixStoreLinux }: {
+      nixStoreLinux = "/home/runner/nix";
+      nixStoreAuto = "auto";
+
+      # Set the custom store to enable caching
+      # https://nixos.org/manual/nix/unstable/command-ref/conf-file.html?highlight=conf#conf-store
+      # Keep build outputs to garbage collect at the end only the trash
+      # https://nixos.org/manual/nix/unstable/command-ref/conf-file.html#description
+      installNix = { store ? nixStoreAuto }: {
         name = "Install Nix";
         uses = "cachix/install-nix-action@v20";
         "with" = {
@@ -152,8 +162,10 @@
             substituters = https://cache.nixos.org/ https://cache.iog.io https://nix-community.cachix.org https://deemp.cachix.org 
             trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18=
             store = ${store}
+            keep-outputs = true
           '';
-          install_url = "https://releases.nixos.org/nix/nix-2.14.1/install";
+          # https://releases.nixos.org/?prefix=nix
+          install_url = "https://releases.nixos.org/nix/nix-2.16.1/install";
         };
       };
 
@@ -179,21 +191,24 @@
             git config user.email github-actions@github.com
           '';
         };
-        updateLocksAndCommit = let name = "Update flake locks"; in
+        updateLocksAndCommit =
+          let name = "Update flake locks"; in
           {
             inherit name;
             run = run.nixRunAndCommit names.updateLocks name;
           };
+        nixStoreCollectGarbage = {
+          name = "Collect garbage in /nix/store";
+          run = "nix store gc";
+        };
       };
-
-      nixStoreLinux = "/home/runner/nix";
 
       strategies = {
         # setting the store doesn't work for macOS
         # https://discourse.nixos.org/t/how-to-use-a-local-directory-as-a-nix-binary-cache/655
         nixCache.matrix.include = [
-          { os = os.macos-11; }
-          { os = os.macos-12; }
+          { os = os.macos-11; store = nixStoreAuto; }
+          { os = os.macos-12; store = nixStoreAuto; }
           { os = os.ubuntu-20; store = nixStoreLinux; }
           { os = os.ubuntu-22; store = nixStoreLinux; }
         ];
@@ -239,7 +254,7 @@
           stepsIf mkAccessors mkAccessors_ run nixCI_ installNix;
       };
       configs = {
-        inherit oss os names on steps nixCI strategies nixStoreLinux;
+        inherit oss os names on steps nixCI strategies nixStoreLinux nixStoreAuto;
       };
     });
 }
