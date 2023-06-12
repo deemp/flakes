49287a0dc5bcc48fdd3b7785c583277e9e7bbacc
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-12 14:36:32 +0300

    keep derivations explicitly

--- workflows/flake.nix
+++ workflows/flake.nix
@@ -165,6 +165,7 @@
             trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18=
             store = ${store}
             keep-outputs = true
+            keep-derivations = true
           '';
           # https://releases.nixos.org/?prefix=nix
           install_url = "https://releases.nixos.org/nix/nix-2.16.1/install";
