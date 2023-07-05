f74dd54a8781c3eb1faa29a2deaaacbb226836ec
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-05 18:49:50 +0300

    format

--- codium/nix-files/extensions.nix
+++ codium/nix-files/extensions.nix
@@ -15,142 +15,145 @@ let
         )
         exts));
   mkExtensions = extensionSet: mapAttrs' (x: y: { name = x; value = mkExtensionsGroup extensionSet y; });
-in 
+in
 __foldl' pkgs.lib.attrsets.recursiveUpdate { }
-[(
-mkExtensions vscode-marketplace
-{
-  c-cpp = {
-    ms-vscode = [
-      "cmake-tools"
-      "cpptools-themes"
-      "cpptools"
-    ];
-  };
-  csv = {
-    "mechatroner" = "rainbow-csv";
-  };
-  docker = {
-    exiasr = "hadolint";
-    ms-azuretools = "vscode-docker";
-    ms-vscode-remote = "remote-containers";
-  };
-  fish = {
-    bmalehorn = "vscode-fish";
-  };
-  github = {
-    github = [ 
-      "vscode-pull-request-github" 
-      "vscode-github-actions" ];
-    redhat = "vscode-yaml";
-  };
-  haskell = {
-    haskell = "haskell";
-    justusadam = "language-haskell";
-    redhat = "vscode-yaml";
-    visortelle = "haskell-spotlight";
-  };
-  jupyter = {
-    ms-toolsai = [
-      "jupyter"
-      "jupyter-keymap"
-      "vscode-jupyter-slideshow"
-      "vscode-jupyter-cell-tags"
-    ];
-  };
-  kubernetes = {
-    ipedrazas = "kubernetes-snippets";
-    ms-kubernetes-tools = "vscode-kubernetes-tools";
-    redhat = "vscode-yaml";
-  };
-  markdown = {
-    bierner =
-      [
-        "github-markdown-preview"
-        "markdown-checkbox"
-        "markdown-emoji"
-        "markdown-footnotes"
-        "markdown-preview-github-styles"
-        "markdown-yaml-preamble"
-      ];
-    davidanson = "vscode-markdownlint";
-    yzhang = "markdown-all-in-one";
-  };
-  misc = {
-    gruntfuggly = "todo-tree";
-    mkhl = "direnv";
-    usernamehw = "errorlens";
-  };
-  nix = {
-    jnoortheen = "nix-ide";
-    mkhl = "direnv";
-  };
-  pdf = {
-    tomoki1207 = "pdf";
-  };
-  postgresql = {
-    inferrinizzard = "prettier-sql-vscode";
-  };
-  prolog = {
-    lilr = "swi-lsp";
-  };
-  purescript = {
-    chunsen = "bracket-select";
-    dhall = [
-      "dhall-lang"
-      "vscode-dhall-lsp-server"
-    ];
-    nwolverson = [
-      "ide-purescript"
-      "language-purescript"
-    ];
-    ryuta46 = "multi-command";
-    br4ch1st0chr0n3 = "purs-keybindings";
-  };
-  python = {
-    batisteo = "vscode-django";
-    donjayamanne = "python-extension-pack";
-    kevinrose = "vsc-python-indent";
-    monosans = "djlint";
-    ms-python = "python";
-    njpwerner = "autodocstring";
-    samuelcolvin = "jinjahtml";
-    visualstudioexptteam = "vscodeintellicode";
-  };
-  rescript = {
-    chenglou92 = "rescript-vscode";
-  };
-  sql = {
-    inferrinizzard = "prettier-sql-vscode";
-    mtxr = "sqltools";
-  };
-  terraform = {
-    hashicorp = "terraform";
-  };
-  toml = {
-    tamasfe = "even-better-toml";
-  };
-  typescript = {
-    ms-vscode = "vscode-typescript-next";
-    esbenp = "prettier-vscode";
-  };
-  vlang = {
-    vlanguage = "vscode-vlang";
-  };
-  yaml = {
-    redhat = "vscode-yaml";
-  };
-})
-(mkExtensions vscode-marketplace-release {
-  github = {
-    eamodio = "gitlens";
-  };
-})
-(mkExtensions open-vsx
-{
-  postgresql = {
-    cweijan = "vscode-postgresql-client2";
-  };
-})
+  [
+    (
+      mkExtensions vscode-marketplace
+        {
+          c-cpp = {
+            ms-vscode = [
+              "cmake-tools"
+              "cpptools-themes"
+              "cpptools"
+            ];
+          };
+          csv = {
+            "mechatroner" = "rainbow-csv";
+          };
+          docker = {
+            exiasr = "hadolint";
+            ms-azuretools = "vscode-docker";
+            ms-vscode-remote = "remote-containers";
+          };
+          fish = {
+            bmalehorn = "vscode-fish";
+          };
+          github = {
+            github = [
+              "vscode-pull-request-github"
+              "vscode-github-actions"
+            ];
+            redhat = "vscode-yaml";
+          };
+          haskell = {
+            haskell = "haskell";
+            justusadam = "language-haskell";
+            redhat = "vscode-yaml";
+            visortelle = "haskell-spotlight";
+          };
+          jupyter = {
+            ms-toolsai = [
+              "jupyter"
+              "jupyter-keymap"
+              "vscode-jupyter-slideshow"
+              "vscode-jupyter-cell-tags"
+            ];
+          };
+          kubernetes = {
+            ipedrazas = "kubernetes-snippets";
+            ms-kubernetes-tools = "vscode-kubernetes-tools";
+            redhat = "vscode-yaml";
+          };
+          markdown = {
+            bierner =
+              [
+                "github-markdown-preview"
+                "markdown-checkbox"
+                "markdown-emoji"
+                "markdown-footnotes"
+                "markdown-preview-github-styles"
+                "markdown-yaml-preamble"
+              ];
+            davidanson = "vscode-markdownlint";
+            yzhang = "markdown-all-in-one";
+          };
+          misc = {
+            gruntfuggly = "todo-tree";
+            mkhl = "direnv";
+            usernamehw = "errorlens";
+          };
+          nix = {
+            jnoortheen = "nix-ide";
+            mkhl = "direnv";
+          };
+          pdf = {
+            tomoki1207 = "pdf";
+          };
+          postgresql = {
+            inferrinizzard = "prettier-sql-vscode";
+          };
+          prolog = {
+            lilr = "swi-lsp";
+          };
+          purescript = {
+            chunsen = "bracket-select";
+            dhall = [
+              "dhall-lang"
+              "vscode-dhall-lsp-server"
+            ];
+            nwolverson = [
+              "ide-purescript"
+              "language-purescript"
+            ];
+            ryuta46 = "multi-command";
+            br4ch1st0chr0n3 = "purs-keybindings";
+          };
+          python = {
+            batisteo = "vscode-django";
+            donjayamanne = "python-extension-pack";
+            kevinrose = "vsc-python-indent";
+            monosans = "djlint";
+            ms-python = "python";
+            njpwerner = "autodocstring";
+            samuelcolvin = "jinjahtml";
+            visualstudioexptteam = "vscodeintellicode";
+          };
+          rescript = {
+            chenglou92 = "rescript-vscode";
+          };
+          sql = {
+            inferrinizzard = "prettier-sql-vscode";
+            mtxr = "sqltools";
+          };
+          terraform = {
+            hashicorp = "terraform";
+          };
+          toml = {
+            tamasfe = "even-better-toml";
+          };
+          typescript = {
+            ms-vscode = "vscode-typescript-next";
+            esbenp = "prettier-vscode";
+          };
+          vlang = {
+            vlanguage = "vscode-vlang";
+          };
+          yaml = {
+            redhat = "vscode-yaml";
+          };
+        }
+    )
+    (mkExtensions vscode-marketplace-release {
+      github = {
+        eamodio = "gitlens";
+      };
+    })
+    (mkExtensions open-vsx
+      {
+        postgresql = {
+          cweijan = "vscode-postgresql-client2";
+        };
+      })
 
-]
+  ]
