74f58092bc4f76364a3dd38489762cadfa0430dd
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-22 23:31:10 +0300

    rm locks

--- source-flake/flake.nix
+++ source-flake/flake.nix
@@ -1,20 +1,20 @@
 {
   inputs = {
-    cachix.url = "github:cachix/cachix/f6fe5df6d96e45359a1e5c448272cfc7e0566da0";
-    devshell.url = "github:numtide/devshell/2cf83bb31720fcc29a999aee28d6da101173e66a";
+    nixpkgs.url = "github:NixOS/nixpkgs/ec322bf9e598a510995e7540f17af57ee0c8d5b9";
+    cachix.url = "github:cachix/cachix";
+    devshell.url = "github:numtide/devshell";
     flake-compat = {
-      url = "github:edolstra/flake-compat/b4a34015c698c7793d592d66adbab377907a2be8";
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
     flake-utils.url = "github:numtide/flake-utils";
     lima.url = "github:deemp/lima";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/295dcce52610c93100c4098714eb06032249bd9d";
-    nixpkgs.url = "github:NixOS/nixpkgs/ec322bf9e598a510995e7540f17af57ee0c8d5b9";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     poetry2nix = {
-      url = "github:nix-community/poetry2nix/4f8d61cd936f853242a4ce1fd476f5488c288c26";
+      url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dream2nix.url = "github:nix-community/dream2nix/e68bff6dbd5aa2b9ffae1e12f60c71bf0bee0143";
+    dream2nix.url = "github:nix-community/dream2nix";
     # TODO use purescript from nixpkgs
     nixpkgs-purescript.url = "github:deemp/nixpkgs/purescript";
     terrafix.url = "github:deemp/terrafix";
