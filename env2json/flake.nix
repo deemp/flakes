0807e5b134696c8012622fbe5bbdf206b28cecc8
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-23 15:34:01 +0300

    upd env2json

--- env2json/flake.nix
+++ env2json/flake.nix
@@ -1,60 +1,46 @@
 {
   inputs.flakes.url = "github:deemp/flakes";
-
-  outputs =
-    inputs:
-    let
-      inputs_ =
-        let flakes = inputs.flakes.flakes; in
-        {
-          inherit (flakes.source-flake) flake-utils nixpkgs;
+  outputs = inputs: inputs.flakes.makeFlake {
+    inputs = { inherit (inputs.flakes.all) nixpkgs; };
+    perSystem = { inputs, system }:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        script = ./env-to-json.py;
+        mkJSON = dotenvPath: pkgs.stdenv.mkDerivation {
+          name = "make-json";
+          buildInputs = [ pkgs.python310 ];
+          unpackPhase = "true";
+          installPhase = ''
+            mkdir -p $out
+            python ${script} ${dotenvPath} > $out/.json
+          '';
         };
-
-      outputs = outputs_ { } // { inputs = inputs_; outputs = outputs_; };
-
-      outputs_ =
-        inputs__:
-        let inputs = inputs_ // inputs__; in
-        inputs.flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-          script = ./env-to-json.py;
-          mkJSON = dotenvPath: pkgs.stdenv.mkDerivation {
-            name = "make-json";
+        envToJSONConverter =
+          let
+            name = "env-to-json-converter";
+          in
+          pkgs.stdenv.mkDerivation {
+            inherit name;
             buildInputs = [ pkgs.python310 ];
             unpackPhase = "true";
             installPhase = ''
-              mkdir -p $out
-              python ${script} ${dotenvPath} > $out/.json
+              mkdir -p $out/bin
+              cp ${script} $out/bin/${name}
+              chmod +x $out/bin/${name}
             '';
           };
-          envToJSONConverter =
-            let
-              name = "env-to-json-converter";
-            in
-            pkgs.stdenv.mkDerivation {
-              inherit name;
-              buildInputs = [ pkgs.python310 ];
-              unpackPhase = "true";
-              installPhase = ''
-                mkdir -p $out/bin
-                cp ${script} $out/bin/${name}
-                chmod +x $out/bin/${name}
-              '';
-            };
-          packages = {
-            default = envToJSONConverter;
-          };
-          lib = {
-            inherit mkJSON;
-          };
-          tests = {
-            mkJSONDemo = mkJSON ./app.env;
-          };
-        in
-        {
-          inherit packages lib tests;
-        });
-    in
-    outputs;
+        packages = {
+          default = envToJSONConverter;
+        };
+        lib = {
+          inherit mkJSON;
+        };
+        tests = {
+          mkJSONDemo = mkJSON ./app.env;
+        };
+      in
+      {
+        inherit packages lib tests;
+      };
+  };
 }
