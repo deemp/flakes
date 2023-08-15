64c3f6c293c46f84db08fb4583b084580c95fc5c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-08-15 16:56:12 +0500

    devshell: simplify

--- devshell/flake.nix
+++ devshell/flake.nix
@@ -9,83 +9,62 @@
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
         inherit (inputs.drv-tools.lib.${system}) framedNewlines framed_;
+        inherit (pkgs.lib.lists) flatten imap0;
+        inherit (pkgs.lib) setPrio;
+        inherit (pkgs.lib.attrsets) mapAttrsToList;
+
+        setPrios = xs: imap0 setPrio (flatten xs);
 
         devshell = let devshell_ = ((pkgs.extend inputs.devshell.overlays.default).devshell); in
           devshell_ // {
             mkShell = configuration: devshell_.mkShell (
               configuration // {
                 # bashInteractive - for VSCodium
-                packages =
-                  pkgs.lib.lists.imap0
-                    pkgs.lib.setPrio
-                    (pkgs.lib.lists.flatten ((configuration.packages or [ ]) ++ [ pkgs.bashInteractive ]));
-                commands = (
-                  builtins.map
-                    (c:
-                      {
-                        category = "programs";
-                        help = "listed in `packages` of this devshell";
-                        command = ''
-                          printf "${framedNewlines ''
-                            This is a dummy command just to let help text for this entry
-                            to be present in this devshell's message
-                          ''}"
-                        '';
-                      } // c // {
-                        # append a space to have no name clashes with original executables
-                        name = c.name + (if builtins.hasAttr "command" c then "" else " ");
-                      })
-                    (
-                      (configuration.commands or [ ])
-                      ++ [
-                        {
-                          name = "exit";
-                          category = "general commands";
-                          help = "exit this devshell";
-                        }
-                        {
-                          name = "man";
-                          category = "general commands";
-                          help = "get more info about a command. Example: 'man man'";
-                        }
-                      ]
-                    )
-                );
+                packages = setPrios ((configuration.packages or [ ]) ++ [ pkgs.bashInteractive ]);
+                commands =
+                  (configuration.commands or [ ])
+                  ++ [
+                    {
+                      name = "exit";
+                      category = "general commands";
+                      command = "exit $@";
+                      help = "exit this devshell";
+                    }
+                    {
+                      name = "man";
+                      category = "general commands";
+                      command = "man $@";
+                      help = "get more info about a command. Example: 'man man'";
+                    }
+                  ]
+                ;
               }
             );
           };
 
         inherit (devshell) mkShell;
 
-        # Case: we have several scripts available in `packages` of a flake
-        # And we'd like to present them in a `devShell`
+        # Case: list commands to run packages in a given directory
         mkRunCommandsDir = dir: category: drvs@{ ... }:
-          pkgs.lib.attrsets.mapAttrsToList
+          mapAttrsToList
             (
-              name: value: {
-                name = "nix run ${dir}#${name}";
-                inherit category;
-                help = value.meta.description or "dummy description";
-              }
+              name: drv:
+                {
+                  name = "nix run ${dir}#${name}";
+                  command = " ";
+                  inherit category;
+                  help = drv.meta.description or "dummy description";
+                }
             )
             drvs;
 
-        # Case: we have several scripts available in `packages` of the current flake
-        # And we'd like to present them in a `devShell`
+        # Case: list commands to run packages in the current directory
         mkRunCommands = mkRunCommandsDir ".";
 
-        # Case: we have several programs available in a `devShell`
-        # And we'd like to present them in that `devShell`
-        mkCommands = category: drvs:
-          map
-            (
-              x: {
-                name = x.pname or x.name;
-                help = x.meta.description or "dummy description";
-                inherit category;
-              }
-            )
-            drvs;
+        # Case: provide packages in the same category
+        mkCommands = category: drvs: map (x: { package = x; inherit category; }) (setPrios drvs);
+
+        mkDefaultCommands = commands: map (x: { command = " "; } // x) commands;
 
         packages = {
           awk = pkgs.gawk;
@@ -94,39 +73,36 @@
         };
 
         devShells.default = mkShell {
-          packages = [ pkgs.gawk pkgs.hello pkgs.nodejs_18 pkgs.nodejs_20 ];
           bash = {
             extra = ''
               printf "Hello, World!\n"
             '';
           };
           commands =
-            mkCommands "pkgs" [ pkgs.gawk pkgs.hello ]
-            ++
-            mkRunCommands "run" { inherit (packages) awk testHello; }
-            ++
-            [
-              {
-                # test name collision prevention
-                name = "hello";
-                package = pkgs.hello;
-              }
-              {
-                name = "awk, hello";
-              }
-              {
-                name = "run-hello";
-                category = "scripts";
-                help = "commands having the same category";
-                command = "${pkgs.hello}/bin/hello";
-              }
-              {
-                name = "run-awk";
-                category = "scripts";
-                help = "commands having the same category";
-                command = "${pkgs.gawk}/bin/awk";
-              }
-            ]
+            mkCommands "pkgs" [ pkgs.gawk pkgs.hello pkgs.nodejs_18 pkgs.nodejs_20 ]
+            ++ mkRunCommands "run" { inherit (packages) awk testHello; }
+            ++ mkDefaultCommands
+              [
+                {
+                  name = "hello";
+                  package = pkgs.hello;
+                }
+                {
+                  name = "awk, hello";
+                }
+                {
+                  name = "run-hello";
+                  category = "scripts";
+                  help = "commands having the same category";
+                  command = "${pkgs.hello}/bin/hello";
+                }
+                {
+                  name = "run-awk";
+                  category = "scripts";
+                  help = "commands having the same category";
+                  command = "${pkgs.gawk}/bin/awk";
+                }
+              ]
           ;
         };
       in
@@ -135,6 +111,7 @@
         lib = {
           inherit
             mkCommands
+            mkDefaultCommands
             mkRunCommands
             mkRunCommandsDir
             mkShell
