7410b5a8f750092c2b5537ef8b7e26a68bad91d1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-29 15:01:42 +0300

    refactor: language-tools/purescript

--- language-tools/purescript/default.nix
+++ language-tools/purescript/default.nix
@@ -1,1 +1,16 @@
-(builtins.getFlake "github:deemp/flakes/${(builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flakes.locked.rev}").outputs.makeDefault ./.
+{ system ? builtins.currentSystem
+, pkgs ? (import ../..).inputs.nixpkgs.legacyPackages.${system}
+, drv-tools ? (import ../../drv-tools { inherit system pkgs; })
+}:
+let
+  lib = drv-tools.lib.mergeAttrsRecursive [ builtins pkgs.lib drv-tools.lib ];
+  packages = {
+    inherit (pkgs) dhall-lsp-server nodejs_20 purescript;
+    inherit (pkgs.nodePackages) purescript-language-server purs-tidy bower;
+    spago = pkgs.lib.meta.addMetaAttrs { description = "PureScript build tool and package manager"; } pkgs.spago;
+  };
+  devShells.default = pkgs.mkShell { buildInputs = lib.attrValues packages; };
+in
+{
+  inherit packages devShells;
+}
