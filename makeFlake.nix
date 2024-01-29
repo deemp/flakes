698225b1a23344789d2542ba9a54a5cae45512e5
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-01-29 15:03:03 +0300

    refactor: makeFlake

--- makeFlake.nix
+++ makeFlake.nix
@@ -1,22 +1,27 @@
+let
+  lib = import
+    (
+      let
+        l = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs_2.locked;
+        tar = fetchTarball {
+          url = "https://github.com/nixos/nixpkgs/archive/${l.rev}.tar.gz";
+          sha256 = l.narHash;
+        };
+      in
+      "${tar}/lib"
+    );
+in
 {
   # original inputs
   inputs
 , # systems to generate outputs for
-  systems ? (import ./source-flake).flake-utils.lib.defaultSystems
+  systems ? [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
 , # outputs per system
-  perSystem ? ({ inputs, system }: { })
-, # raw outputs
-  raw ? (inputs: { })
+  perSystem ? _: { }
+, # other outputs
+  other ? { }
 }:
-let
-  # a function where a user can override original inputs
-  inputs_ = inputs;
-  customOutputs = inputs__:
-    let inputs = inputs_ // inputs__; in
-    (import ./source-flake).flake-utils.lib.eachSystem systems (system: perSystem { inherit inputs system; })
-    // raw inputs
-    # save overriden inputs
-    // { inherit inputs; };
-in
-# default outputs with original inputs
-customOutputs { } // { outputs = customOutputs; }
+lib.pipe systems [
+  (map (sys: builtins.mapAttrs (_: value: { ${sys} = value; }) (perSystem sys)))
+  (builtins.foldl' lib.recursiveUpdate other)
+]
